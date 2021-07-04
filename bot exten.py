227d60e431522f6537216adcc8c213bae6d77ddd
docilefiasco <epshita.hs@gmail.com>
2021-07-04 12:27:20 +0530

    final changes added

--- bot exten.py
+++ bot exten.py
@@ -21,16 +21,17 @@ def get_quote():
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return (quote)
 
-    def update_encouragements(encouraging_message):
-        if "encouragement" in db.key():
-            encouragements = db["encouragements"]
-            encouragements.append(encouraging_message)
-            db["encouragements"] = encouragements
-        else:
-            db["encouragements"] = [encouraging_message]
+
+def update_encouragements(encouraging_message):
+    if "encouragements" in db.keys():
+        encouragements = db["encouragements"]
+        encouragements.append(encouraging_message)
+        db["encouragements"] = encouragements
+    else:
+        db["encouragements"] = [encouraging_message]
 
 
-def delete_encouragement(index):
+def delete_encouragment(index):
     encouragements = db["encouragements"]
     if len(encouragements) > index:
         del encouragements[index]
@@ -51,11 +52,24 @@ async def on_ready():
         await message.channel.send(quote)
 
         options = starter_encouragements
-        if "encouragements" in db.key
+        if "encouragements" in db.keys():
+            options = options + db["encouragements"]
 
         if any(word in message.content for word in sad_words):
-            await message.channel.send(random.choice(starter_encouragements))
+            await message.channel.send(random.choice(options))
+
+        if message.content.startswith("$new"):
+            encouraging_message = message.content.split("$new ", 1)[1]
+            update_encouragements(encouraging_message)
+            await message.channel.send("New encouraging message added.")
+
+            if message.content.startswith("$del"):
+                encouragements = []
+        if "encouragements" in db.keys():
+            index = int(message.content.split("$del", 1)[1])
+            delete_encouragment(index)
+            encouragements = db["encouragements"]
+        await message.channel.send(encouragements)
 
 
 client.run(os.environ['TOKEN'])
-
